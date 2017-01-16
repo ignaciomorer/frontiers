8f891a4496ef5c5f530ee40cbf7d7cc04ec5d17c
ignaciomorer <ignaciomorer@users.noreply.github.com>
2017-01-16 12:22:26 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,64 @@
 
+def real_distance(G,node1,node2):
+    from math import sin, cos, radians, asin, sqrt
+    lon0=G.node[node1]['longitude']
+    lat0=G.node[node1]['latitude']
+    lon1=G.node[node2]['longitude']
+    lat1=G.node[node2]['latitude']
+    lon0, lat0, lon1, lat1 = map(radians, [lon0, lat0, lon1, lat1])
+    # haversine formula
+    dlon = lon1 - lon0
+    dlat = lat1 - lat0
+    a = sin(dlat/2)**2 + cos(lat0) * cos(lat1) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    # 6367 is the radius of the Earth
+    km = 6367 * c
+    return km
 
+def distance_matrix(G):
+    D = []
+    for i in range(len(G.nodes())):
+        D+=[[]]
+        for j in range(len(G.nodes())):
+            if i==j:
+                d= 0
+            else:
+                d = real_distance(G,G.nodes()[i],G.nodes()[j])
+            D[i]+=[(d,j)]
+            #D[i]+=[d]
+    return D
+
+def min_distances_tree(G):
+    import networkx as nx
+    Gm=G.copy()
+    Gm.remove_edges_from(G.edges())
+    D=distance_matrix(Gm)
+    D_list=[]
+    for i in range(len(D)):
+        j = 0
+        while j < i:
+            dist=D[i][j]
+            D_list +=[(dist,i,j)]
+            j += 1
+
+    D_list=sorted(D_list)
+    i=0
+    edge_count=0
+    ncc=nx.number_connected_components(Gm)
+    while not ncc==1:
+        node1=Gm.nodes()[D_list[i][1]]
+        node2=Gm.nodes()[D_list[i][2]]
+        if not nx.has_path(Gm,node1,node2):
+            d=D_list[i][0][0]
+            edge_count+=1
+            Gm.add_edge(node1,node2,weight=d,time=edge_count)
+            ncc=nx.number_connected_components(Gm)
+        i+=1
+
+    return Gm
 
 def global_efficiency(G):
     import networkx as nx
-    from utilities import real_distance
     # Matriz de distancias ordenadas segun lista G.nodes()
     distances=[]
     N=len(G.nodes())
